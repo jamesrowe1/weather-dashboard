e47580b97a210fcceee84fa41fb40818da12349d
James Rowe <jamesrowe@outlook.com>
2020-07-13 20:41:29 -0400

    got weather data

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
+//DOM Elements
+var cityHistoryEl = $("#history");
+
+//Initialize
+var cityHistory = [];
+var temperature;
+var humidity;
+var windSpeed;
+var city;
+
+//need some extras for uvIndex
+var long;
+var lat;
+var uvIndex;
+
 $("#searchButton").on("click", function (event) {
   event.preventDefault();
-  var city = "Hershey";
+
+  //get the city input from the input box
+  //   city = $("#cityInput").val();
+  city = "Hershey";
+  //check that something was input. If not, return
+  if (city === "") {
+    return;
+  }
+  //get the weather info of the city
   var queryURL =
-    "api.openweathermap.org/data/2.5/weather?q=" +
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
     city +
-    "&appid={5907a64e4c4cf40021f641c3ddf19281}";
+    "&units=imperial&appid=5907a64e4c4cf40021f641c3ddf19281";
   //ajax to call the above URL for the city
   $.ajax({
     url: queryURL,
@@ -12,4 +35,5 @@ $("#searchButton").on("click", function (event) {
   }).then(function (response) {
     console.log(response);
   });
+  cityHistoryEl.prepend("<li>" + city + "</li>");
 });
