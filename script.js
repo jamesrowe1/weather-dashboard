4e4e6e459449c0a5b0e561fc233d9d210f2716b7
James Rowe <jamesrowe@outlook.com>
2020-07-13 22:11:58 -0400

    save all i need

--- script.js
+++ script.js
@@ -24,16 +24,44 @@ $("#searchButton").on("click", function (event) {
     return;
   }
   //get the weather info of the city
-  var queryURL =
+  var queryCityURL =
     "https://api.openweathermap.org/data/2.5/weather?q=" +
     city +
     "&units=imperial&appid=5907a64e4c4cf40021f641c3ddf19281";
   //ajax to call the above URL for the city
   $.ajax({
-    url: queryURL,
+    url: queryCityURL,
     method: "GET",
   }).then(function (response) {
     console.log(response);
+    //grab the info we just got
+    temperature = response.main.temp;
+    humidity = response.main.humidity;
+    windSpeed = response.wind.speed;
+    //getting the longitude and latitude
+    lon = response.coord.lon;
+    lat = response.coord.lat;
+
+    //using lat and lon to get the UV index
+    var queryUVURL =
+      "http://api.openweathermap.org/data/2.5/uvi?appid=5907a64e4c4cf40021f641c3ddf19281&lat=" +
+      lat +
+      "&lon=" +
+      lon;
+    $.ajax({
+      url: queryUVURL,
+      method: "GET",
+    }).then(function (uvResponse) {
+      console.log(uvResponse);
+      uvIndex = uvResponse.value;
+
+      //async is annoying
+      console.log(temperature);
+      console.log(humidity);
+      console.log(windSpeed);
+      console.log(uvIndex);
+    });
   });
+
   cityHistoryEl.prepend("<li>" + city + "</li>");
 });
